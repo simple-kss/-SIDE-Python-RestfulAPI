9414fa0e783c9d1dc2e7cbfdde665fd417050608
Seongsik Kim <kss7858@naver.com>
2021-01-29 00:09:26 +0900

    ADD: Class of InputSensor and Outputsensor
    Signed-off-by: Seongsik Kim <kss7858@naver.com>

---
+++ project/InputSensor.py
@@ -0,0 +1,20 @@
+# HAL (Hardware Abstract Layer)
+
+class InputSensor:
+    pin = 0
+    val = 0
+
+    def __init__(self):
+        pass
+
+    def isDownAtSensor(self):
+        pass
+
+    def isUpAtSensor(self):
+        pass
+
+    def isLowerthan(self):
+        pass
+
+    def isHigerthan(self):
+        pass
